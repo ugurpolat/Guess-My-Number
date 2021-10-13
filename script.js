538c6d1b3a1b21827df33c08eded35ddf421b0c3
Ugur Polat <ugurpolat113@gmail.com>
2021-10-13 23:33:32 +0300

    Added Mastermind logic.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 var secretNumber = Math.trunc(Math.random() * 20) + 1;
 var score = 20;
 var highScore = 0;
@@ -47,3 +47,42 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.message').textContent = 'Start guessing...';
   document.querySelector('body').style.backgroundColor = '#000';
 });
+*/
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]; //Math.trunc(Math.random() * 9) + 1;
+let guessNumber = [];
+
+let guessDigitsNumber = prompt('How many digits do you want?');
+console.log(guessDigitsNumber);
+
+let playerNumber = prompt('What is your digits?');
+let setArr = num => Number(num);
+let playerArray = Array.from(String(playerNumber), setArr);
+
+const createRandomNumber = function (guessDigitsNumber) {
+  let i = 0;
+
+  while (i < guessDigitsNumber) {
+    let randomIndex = Math.trunc(Math.random() * numbers.length);
+    let value = numbers[randomIndex];
+    if (value !== 0) {
+      guessNumber[i] = value;
+      numbers.splice(randomIndex, 1);
+      i++;
+    } else {
+      continue;
+    }
+  }
+
+  return guessNumber;
+};
+
+console.log(createRandomNumber(guessDigitsNumber));
+console.log(playerArray);
+
+for (let i = 0; i < guessDigitsNumber; i++) {
+  if (guessNumber[i] == playerArray[i]) {
+    console.log('+1');
+  } else {
+    console.log('-1');
+  }
+}
